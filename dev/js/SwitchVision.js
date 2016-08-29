a5ef6872eed131bda046f2af0be5b0d29a08012c
Andrey Eremenko <fr0ntender@yandex.ru>
2016-08-29 08:33:10 +0300

    Сделана индексаня, раздел в работе

--- dev/js/SwitchVision.js
+++ dev/js/SwitchVision.js
@@ -1,14 +1,50 @@
+//Переключает видимость компонента при изменении паметра this.options.prop
+//this.options.showClass - класс, показывающий блок
+//публичный метод close закрывает окно
+// Можно передать объект с настройками анимации:
+// animate.attr - анимируемый атрибут
+// animate.showSeed - скорость анимации показа элемента (по умолчания 1000)
+// animate.hideSeed - скорость анимации сокрытия элемента (по умолчания х3 скорости анимации)
+
 Component.create('SwitchVision', {
+    events: ['click'],
     init: function () {
         var prop = this.options.prop,
-            showClass = this.options.showClass;
+            showClass = this.options.showClass,
+            animate = this.options.animate || {},
+            showSpeed = animate.showSeed || 1000;
 
-        this.controller.on('change', prop, function (show) {
+        this.on('change', prop, function (show) {
             if(show) {
-                this.$el.addClass(showClass);
+                showElement.apply(this);
             } else {
-                this.$el.removeClass(showClass);
+                hideElement.apply(this);
             }
-        }, this);
+        });
+
+        function showElement() {
+            if(showClass) {
+                this.$el.addClass(showClass);
+            } else if (animate) {
+                var attr = {};
+                attr[animate.attr] = 'show';
+                this.$el.animate(attr,showSpeed);
+            }
+        }
+
+        function hideElement() {
+            if(showClass) {
+                this.$el.removeClass(showClass);
+            } else if (animate) {
+                var attr = {};
+                attr[animate.attr] = 'hide';
+                this.$el.animate(attr, animate.hideSpeed || (showSpeed / 3));
+            }
+        }
+    },
+    close: function () {
+        var obj = {};
+        obj[this.options.prop] = false;
+        this.controller.set(obj);
     }
 });
